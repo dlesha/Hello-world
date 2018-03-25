213891fb0e0343e6c150dae5dfb48a44dabf9dbb
dlesha <37777533+dlesha@users.noreply.github.com>
2018-03-25 19:45:02 +0300

    Added HelloWorld project

---
+++ HelloWorld/src/CalculationClass.java
@@ -0,0 +1,12 @@
+
+public class CalculationClass {
+
+	public CalculationClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer Calc(Integer a, Integer b)
+	{
+		return a + b;
+	}
+}
